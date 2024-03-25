de8ff4ba7fd77c1ac80da9a864e59fa14bf1ae36
Akos Melczer <webmastertd000@gmail.com>
2024-03-25 20:57:11 +0000

    Added getrange api

--- Server/app/api/db.php
+++ Server/app/api/db.php
@@ -2,8 +2,6 @@
 
 namespace DB;
 
-
-
 $_conn = null;
 function conn() {
 	global $_conn;
@@ -22,7 +20,15 @@ function conn() {
 }
 
 function post_data($timestamp, $temperature, $humidity, $rainfall) {
-	$insert = conn() -> prepare("INSERT INTO MeasuredData (timestamp, temperature, humidity, rainfall) VALUES(:timestamp, :temperature, :humidity, :rainfall);");
+	$table = "MeasuredData";
+	$insert = conn() -> prepare("INSERT INTO $table (timestamp, temperature, humidity, rainfall) VALUES(:timestamp, :temperature, :humidity, :rainfall);");
 	$insert -> execute([":timestamp" => $timestamp->format('Y-m-d H:i:s'), ":temperature" => $temperature, ":humidity" => $humidity, ":rainfall" => $rainfall]);
 	return conn() -> lastInsertId();
+}
+
+function get_data($start, $end) {
+	$table = "MeasuredData";
+	$select = conn() -> prepare("SELECT * FROM $table WHERE `timestamp` > :start  and `timestamp` < :end ;");
+	$select -> execute([":start" => $start->format('Y-m-d H:i:s'), ":end" => $end->format('Y-m-d H:i:s')]);
+	return $select -> fetchAll(\PDO::FETCH_ASSOC);
 }
--- Server/app/api/endpoints.php
+++ Server/app/api/endpoints.php
@@ -3,7 +3,7 @@
 require_once 'db.php';
 use function Oink\{datetime, number, check};
 
-function post_data() {
+function data_post() {
 	$timestamp = datetime("timestamp");
 	$temperature = number("temperature");
 	$humidity = number("humidity");
@@ -11,13 +11,8 @@ function post_data() {
 	DB\post_data($timestamp, $temperature, $humidity, $rainfall);
 }
 
-function data_get() {
-	$timestamp = datetime("timestamp");
-	$temperature = number("temperature");
-	$humidity = number("humidity");
-	$rainfall = number("rainfall");
-
-	$start = = datetime("start", default: "0000-01-01 00:00:00");
-	$end= datetime("end", default: date('Y-m-d H:i:s'));
-	DB\post_data($timestamp, $temperature, $humidity, $rainfall);
+function data_getrange() {
+	$start = datetime("start", format: "Y-m-d H:i:s", default: date_create('0000-01-01'), optional: true);
+	$end = datetime("end", format: "Y-m-d H:i:s", default: date_create(), optional: true);
+	return DB\get_data($start, $end);
 }
